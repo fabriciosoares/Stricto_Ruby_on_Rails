55184a5a0deba7c35d4487d9c3ec08601488fd3a
suporte <suporte@192.168.1.5>
2015-05-20 19:46:41 -0300

    Commit inicial do Ruby on Rails

---
+++ codecademy/tarefa_02.rb
@@ -0,0 +1,28 @@
+# Captura um texto do usuário e depois captura um grupo
+# de palavras e substitui estas palavras no texto inicial
+# pela palavra REDACTED
+
+puts "digite um texto: "
+text = gets.chomp
+text.downcase!
+puts "digite as palavras para busca: "
+redact = gets.chomp
+redact.downcase!
+words = text.split(" ")
+redact_split = redact.split(" ")
+resultado = ""
+marca = 0
+words.each do |words|
+	redact_split.each do |redact_split|
+		if words == redact_split
+			marca = 1
+		end
+	end
+	if marca == 1
+		resultado += " REDACTED"
+	else
+		resultado += " " + words
+	end
+	marca = 0
+end
+print resultado
---
+++ codecademy/tarefa_03.rb
@@ -0,0 +1,17 @@
+# Lê uma frase do usuário, contabiliza as palavras e ordena 
+# as palavras pela quantidade de incidências
+
+puts "Entre com uma frase:"
+text = gets.chomp
+words = text.split(" ")
+frequencies = Hash.new(0)
+words.each do |palavra|
+	frequencies[palavra] += 1
+end
+frequencies = frequencies.sort_by do |palavra, qtd|
+	qtd
+end
+frequencies.reverse!
+frequencies.each do |palavra, qtd|
+	puts palavra + " " + qtd.to_s
+end
